6f38fc39156dbfe6b7bda4a605e7cf1a620a61f3
Raman Gupta <gr.gupta.raman@gmail.com>
2024-06-29 11:20:06 -0400

    feat: add api route for getting courses by term and course code

---
+++ src/app/api/v1/terms/[term]/courses/[courseCode]/route.ts
@@ -0,0 +1,19 @@
+import supabase from "@/supabase/supabase";
+
+export async function GET(req: Request, context: any) {
+  const { params } = context;
+
+  const termParam = params.term.trim();
+  const courseCodeParam = params.courseCode.trim().toUpperCase();
+
+  const res = await supabase.rpc("get_course", {
+    termParam,
+    courseCodeParam,
+  });
+
+  if (!res.data) {
+    return Response.json({ error: "No course found", data: null });
+  }
+
+  return Response.json({ error: null, data: res.data[0] });
+}
