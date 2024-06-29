0ce8949cd5904e8cf015cd83862888aa44259751
Raman Gupta <gr.gupta.raman@gmail.com>
2024-06-29 18:28:55 -0400

    refactor: extract HTML to CourseResult component

---
+++ src/components/SearchResults/CourseResult/CourseResult.tsx
@@ -0,0 +1,36 @@
+import { Course } from "@/types/Course";
+import Image from "next/image";
+import { useState } from "react";
+
+interface CourseResultProps {
+  course: Course;
+}
+
+const CourseResult: React.FC<CourseResultProps> = ({ course }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      key={`${course.courseCode}${course.term}`}
+      className="p-2 rounded-sm bg-red-500"
+    >
+      <div className="flex items-center justify-between">
+        <div className="truncate">
+          <span>{`${course.courseCode}: ${course.courseTitle}`}</span>
+        </div>
+
+        <button onClick={() => setIsOpen((is) => !is)}>
+          <Image
+            className={isOpen ? "-rotate-90" : "rotate-0"}
+            width={24}
+            height={24}
+            src={"/chevron-left.svg"}
+            alt="Expand"
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CourseResult;
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,35 +1,19 @@
 import { useCourses } from "@/contexts/CourseContext";
 import React, { useState } from "react";
 import Image from "next/image";
+import CourseResult from "./CourseResult/CourseResult";
 
 export default function SearchResults() {
-  const [isOpen, setIsOpen] = useState(false);
   const { courses } = useCourses();
 
   return (
     <>
-      {courses.map((elem) => {
+      {courses.map((course) => {
         return (
-          <div
-            key={`${elem.courseCode}${elem.term}`}
-            className="p-2 rounded-sm bg-red-500"
-          >
-            <div className="flex items-center justify-between">
-              <div className="truncate">
-                <span>{`${elem.courseCode}: ${elem.courseTitle}`}</span>
-              </div>
-
-              <button onClick={() => setIsOpen((is) => !is)}>
-                <Image
-                  className={isOpen ? "-rotate-90" : "rotate-0"}
-                  width={24}
-                  height={24}
-                  src={"/chevron-left.svg"}
-                  alt="Expand"
-                />
-              </button>
-            </div>
-          </div>
+          <CourseResult
+            key={`${course.courseCode}${course.term}`}
+            course={course}
+          />
         );
       })}
     </>
