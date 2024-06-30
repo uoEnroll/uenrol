f8a66485e4424eb316d204880b172c94e2d72d04
Raman Gupta <gr.gupta.raman@gmail.com>
2024-06-29 20:42:21 -0400

    feat: add slight border around course result

--- src/components/SearchResults/CourseResult/CourseResult.tsx
+++ src/components/SearchResults/CourseResult/CourseResult.tsx
@@ -14,33 +14,35 @@ const CourseResult: React.FC<CourseResultProps> = ({ course }) => {
 
   return (
     <div className="px-4 pb-4">
-      <div className="p-2 rounded-sm bg-red-500">
-        <div className="flex items-center justify-between">
-          <div className="truncate">
-            <span>{`${course.courseCode}: ${course.courseTitle}`}</span>
-          </div>
+      <div className="border rounded-md overflow-hidden">
+        <div className="p-2 bg-red-500">
+          <div className="flex items-center justify-between">
+            <div className="truncate">
+              <span>{`${course.courseCode}: ${course.courseTitle}`}</span>
+            </div>
 
-          <button onClick={() => setIsOpen((is) => !is)}>
-            <FontAwesomeIcon
-              className={`transition-all ease-in delay-100 ${isOpen ? "-rotate-90" : "rotate-0"}`}
-              icon={faChevronLeft}
-            />
-          </button>
-        </div>
-      </div>
-      {course.sections.map((section) => {
-        return (
-          <div
-            className={`transition-all ease-in delay-100 ${isOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}
-            key={`${course.courseCode}${course.term}${section.section}`}
-          >
-            <SectionResult
-              partialKey={`${course.courseCode}${course.term}`}
-              section={section}
-            />
+            <button onClick={() => setIsOpen((is) => !is)}>
+              <FontAwesomeIcon
+                className={`transition-all ease-in delay-100 ${isOpen ? "-rotate-90" : "rotate-0"}`}
+                icon={faChevronLeft}
+              />
+            </button>
           </div>
-        );
-      })}
+        </div>
+        {course.sections.map((section) => {
+          return (
+            <div
+              className={`transition-all ease-in delay-100 ${isOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}
+              key={`${course.courseCode}${course.term}${section.section}`}
+            >
+              <SectionResult
+                partialKey={`${course.courseCode}${course.term}`}
+                section={section}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
